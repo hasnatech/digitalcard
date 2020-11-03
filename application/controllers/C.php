ba6d41ea71d4444f38cdc09df2387d0fae8d7926
unknown <wahithbe407@gmail.com>
2020-11-03 11:10:51 +0530

    c

---
+++ application/controllers/C.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class C extends MY_Controller
+{
+
+	public function site($id)
+	{
+        //echo $id;
+		$this->render('ws/website');
+    }
+
+}
